040ad7728b24752d9fe190acf5b8cd7001ee6b65
leeyoungsub <liive29252@gmail.com>
2020-06-27 23:51:08 +0900

    200627

--- CopyReference/Program.cs
+++ CopyReference/Program.cs
@@ -2,11 +2,36 @@
 
 namespace CopyReference
 {
+    //객체
+    //속성
+    //기능
+
+    //참조
+    class Knight
+    {
+        public int hp;
+        public int attack;
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");
+        }
+        public void Attack()
+        {
+            Console.WriteLine("knight Attack");
+        }
+    }
+    //복사
+    class mage
+    {
+        public int hp;
+        public int attack;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Knight knight = new Knight();
+            
         }
     }
 }
