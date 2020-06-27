edc205d227a7180039feb6d2ff7e47dbe849dac6
leeyoungsub <liive29252@gmail.com>
2020-06-28 00:10:53 +0900

    200628

---
+++ StaticIdebtity/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StaticIdebtity
+{
+    class Knight
+    {
+        static public int count=1;//오로지 한개만 존재
+        public int id;
+        public int hp;
+        public int attack;
+        public Knight()
+        {
+            id = count;
+            count++;
+            hp = 100;
+            attack = 10;
+            Console.WriteLine("생성자 호출");
+        }
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");
+        }
+        public void Attack()
+        {
+            Console.WriteLine("knight Attack");
+        }
+    }
+    //복사
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight1 = new Knight();
+            Knight knight2= new Knight();
+            knight2.hp = 80;
+            Knight knight3 = new Knight();
+            knight3.hp = 200;
+
+        }
+    }
+}
