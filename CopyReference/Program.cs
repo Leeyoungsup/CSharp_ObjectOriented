1973019617dfcf2e9fd3fa99a74a62869683c9fd
leeyoungsub <liive29252@gmail.com>
2020-06-27 17:45:42 +0900

    200627

---
+++ CopyReference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CopyReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ObjectOrientedStart/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ObjectOrientedStart
+{
+    //객체
+    //속성
+    //기능
+    class Knight
+    {
+        public int hp;
+        public int attack;
+        public void Move()
+        {
+            Console.WriteLine("Knight Move") ;
+        }
+        public void Attack()
+        {
+            Console.WriteLine("knight Attack");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            knight.attack = 10;
+            knight.hp = 100;
+            knight.Move();
+            knight.Attack();
+        }
+    }
+}
