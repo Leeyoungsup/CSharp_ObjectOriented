f58c6922b0a21adda6e1a499c841afa68835ccd8
leeyoungsub <liive29252@gmail.com>
2020-06-27 23:58:03 +0900

    200627

--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -2,11 +2,48 @@
 
 namespace Constructor
 {
+    class Knight
+    {
+        public int hp;
+        public int attack;
+        public Knight()
+        {
+            hp = 100;
+            attack = 10;
+            Console.WriteLine("생성자 호출");
+        }
+        public Knight(int hp) : this()
+        {
+            this.hp = hp;
+            Console.WriteLine("int 생성자 호출");
+        }
+        public Knight(int hp,int attack) : this()
+        {
+            this.hp = hp;
+            this.attack = attack;
+            Console.WriteLine("int, int 생성자 호출");
+        }
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");
+        }
+        public void Attack()
+        {
+            Console.WriteLine("knight Attack");
+        }
+    }
+    //복사
+    class mage
+    {
+        public int hp;
+        public int attack;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Knight knight = new Knight(50,5);
+
         }
     }
 }
