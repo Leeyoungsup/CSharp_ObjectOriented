73ce0eda5b9e1dd10a21283c46da1ce33c45cf3a
leeyoungsub <liive29252@gmail.com>
2020-06-28 21:06:51 +0900

    200628

---
+++ ClassTypeConversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassTypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- encapsulation/Program.cs
+++ encapsulation/Program.cs
@@ -6,11 +6,28 @@ namespace encapsulation
     //자동차
     //핸들 페달 차문
     //전기장치 엔진 기름
+    class Knight
+    {
+        //접근 한정자 
+        //public 모든권한
+        //protected 자식클래스와 본인만 사용가능
+        //아무것도 없을 경우 private
+        //private 본인만 사용 가능
+        protected int hp;
+        
+    }
+    class SuperKnight:Knight
+    {
+        void Test()
+        {
+            hp = 10;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Knight knight = new Knight();
         }
     }
 }
